35268b50abb91d8b3577acba8139e523cd4de452
Olaoluwa I. Nwobodo <olaoluwa.nwobodo@aun.edu.ng>
2022-10-06 15:51:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function getDivWidth() {
-  alert(document.querySelector("#box-right").clientWidth);
+  alert(document.querySelector(".container").clientWidth);
+}
+
+window.onresize = () => {
+  document.querySelector("#screenwidth").textContent = window.innerWidth + "px";
+};
+
+window.onload = () => {
+  document.querySelector("#screenwidth").textContent = window.innerWidth + "px";
+};
+
+
+function showUpdatedDate() {
+  document.querySelector("#updated").textContent = new Date();
 }
